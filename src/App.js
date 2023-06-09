f81c5d162ec0f6994f1f0ef1e3ac193b081772fd
LalanBhuwan <bhuwanlalan475@gmail.com>
2023-06-09 22:08:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Topics covered</h1>
+      <p>
+        The following is a list of all the topics we cover in the MDN learning
+        area.
+      </p>
+      <br />
+      
+      <a href="https://www.w3schools.com">Getting started with the web</a>
+      <p>provides a practical introduction to web development for complete beginners.</p>
+      <br />
+      <a href="https://www.w3schools.com">HTML - Structuring the web</a>
+      <p>
+        HTML is the language that we use to structure the different parts of our
+        content and define what their meaning or purpose is. This topic teaches
+        HTML in detail.
+      </p>
+      <br />
+      <a href="https://www.w3schools.com">CSS - Stryling the web</a>
+      <p>
+        CSS is the language that we use to control our web content's style and
+        layout, as well as adding behaviour like animation. This topic provides
+        comprehensive coverage of CSS.
+      </p>
     </div>
   );
 }
